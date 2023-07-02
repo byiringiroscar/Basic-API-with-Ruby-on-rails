3302fb50276ecbc1e142c4f5a5a0dfaf4a4a65d1
byiringiroscar <byiringoroscar@gmail.com>
2023-07-03 01:15:27 +0200

    Add routings

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  resources :bands do
-    resources :members
+
+  namespace :api do
+    namespace :v1 do
+      resources :bands do
+        resources :members
+      end
+    end
+
+    namespace :v2 do
+      resources :members
+      resources :bands
+    end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
